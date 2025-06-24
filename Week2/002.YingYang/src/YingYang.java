6f39fd216344781a921397547af5101fa84714ba
Mohammed <m.lahlal@student.avans.nl>
2025-06-24 17:26:22 +0200

    2.2 gefixt alleen moet ik nog op csg manier

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -8,6 +8,7 @@ import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -27,14 +28,35 @@ public class YingYang extends Application {
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
 
-
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-    }
 
+        graphics.setColor(Color.black);
+        Ellipse2D.Double circle1 = new Ellipse2D.Double(150,100,200,200);
+        graphics.draw(circle1);
+
+        Arc2D.Double arc1 = new Arc2D.Double(150,100,200,200,270,180,Arc2D.OPEN);
+        graphics.fill(arc1);
+
+        graphics.setColor(Color.white);
+        Ellipse2D.Double circle2 = new Ellipse2D.Double(200,100,100,100);
+        graphics.fill(circle2);
+
+        graphics.setColor(Color.black);
+        Ellipse2D.Double circle3 = new Ellipse2D.Double(200,200,100,100);
+        graphics.fill(circle3);
+
+        graphics.setColor(Color.black);
+        Ellipse2D.Double circle4 = new Ellipse2D.Double(240,130,30,30);
+        graphics.fill(circle4);
+
+        graphics.setColor(Color.white);
+        Ellipse2D.Double circle5 = new Ellipse2D.Double(240,230,30,30);
+        graphics.fill(circle5);
+    }
 
     public static void main(String[] args)
     {
